5829686e3ffdf6ea2e7652b6f11a1d406f35bf05
Nasib <nasib4patel@gmail.com>
2024-01-07 12:34:19 +0530

    thirdd commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,18 @@
-echo " BUILD START"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
-echo " BUILD END"
+#!/bin/bash
+
+echo "BUILD START"
+
+# Check Python version path
+PYTHON_PATH=$(which python3.10)
+echo "Python 3.10 Path: $PYTHON_PATH"
+
+# Check Django version
+$PYTHON_PATH -m django --version
+
+# Install dependencies
+$PYTHON_PATH -m pip install -r requirements.txt
+
+# Run collectstatic
+$PYTHON_PATH manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
